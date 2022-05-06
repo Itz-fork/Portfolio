b5a1617633a82e7a188d41c56c0c169b42d6a665
Itz-fork <git.itz-fork@gmail.com>
2022-05-06 09:37:44 +0530

    Added fade-in animation to proejct cards

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Saos from "saos/src/Saos.svelte";
+
 	export let repo = {
 		name: "",
 		description: "",
@@ -9,22 +11,45 @@
 </script>
 
 <main>
-	<div class="card w-96 bg-base-100 shadow-xl">
-		<div class="card-body">
-            <!-- Repo name -->
-			<h2 class="card-title">{repo.name}</h2>
-            <!-- Repo description -->
-			<p>{repo.description}</p>
-            <div id="justify-start">
-                <span class="font-semibold">⭐ Stars:</span> {repo.stargazers_count}
-                <span class="font-semibold">🍴 Forks:</span> {repo.forks_count}
-            </div>
-			<div class="card-actions justify-end">
-                <!-- Check out button -->
-				<a href={repo.html_url} target="_blank"
-					><button class="btn btn-primary">Check out!</button></a
-				>
+	<Saos animation={"fade-in 1.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both"}>
+		<div class="card w-96 bg-base-100 shadow-xl">
+			<div class="card-body">
+				<!-- Repo name -->
+				<h2 class="card-title">{repo.name}</h2>
+				<!-- Repo description -->
+				<p>{repo.description}</p>
+				<div id="justify-start">
+					<span class="font-semibold">⭐ Stars:</span>
+					{repo.stargazers_count}
+					<span class="font-semibold">🍴 Forks:</span>
+					{repo.forks_count}
+				</div>
+				<div class="card-actions justify-end">
+					<!-- Check out button -->
+					<a href={repo.html_url} target="_blank"
+						><button class="btn btn-primary">Check out!</button></a
+					>
+				</div>
 			</div>
 		</div>
-	</div>
+	</Saos>
 </main>
+
+<style>
+	/* Copied from https://github.com/shiryel/saos/blob/master/demo/src/Animations.svelte */
+	/* ----------------------------------------------
+ * Animations below by Animista on 2020-7-11
+ * Licensed under FreeBSD License.
+ * See http://animista.net/license for more info. 
+ * w: http://animista.net, t: @cssanimista
+ * ---------------------------------------------- */
+	@keyframes -global-fade-in {
+		0% {
+			opacity: 0;
+		}
+
+		100% {
+			opacity: 1;
+		}
+	}
+</style>
